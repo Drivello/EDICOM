c4a70a81eb42935e30282aac511c63b445c5db07
Brian <brianrosarino@gmail.com>
2021-06-08 12:58:10 -0300

    Route and Controller /buildings/all

--- api/src/routes/buildings.js
+++ api/src/routes/buildings.js
@@ -2,11 +2,13 @@ const router = require('express').Router();
 const express = require('express');
 
 
-const allAmenities = require("../controllers/amenities/addAmenityController");     // importando todos los amenities
+const allBuildings = require("../controllers/buildings/allBuildings");     // import the controller to show all buildings
+const addBuilding = require("../controllers/buildings/allBuildings");     // import the controller to show all buildings
 
 router.use(express.json());
 
-router.get("/all", allAmenities);       //      localhost3001/amenities/all
+router.get("/all", allBuildings);       //      get -> localhost3001/buildings/all
+router.post("/", addBuilding);       //      post -> localhost3001/buildings
 
 
 router.get('/', async function(req,res,next){ // endPoint
