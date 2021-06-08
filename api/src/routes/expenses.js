7af41d3130bf15cf71ae1422e624fc15ec87cd97
MauriiAC <mauriciocuello91@gmail.com>
2021-06-08 15:53:40 -0300

    file of expenses routes created

---
+++ api/src/routes/expenses.js
@@ -0,0 +1,11 @@
+const router = require('express').Router();
+const express = require('express');
+
+
+
+
+router.get('/', async function(req,res,next){ // endPoint
+    console.log("Estoy en la ruta /expenses")
+})
+
+module.exports = router;
