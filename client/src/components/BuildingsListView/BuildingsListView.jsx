984c3a769376c63df09b55d35eced8fb9f270e25
Leopoldo <leopoldoblanco@gmail.com>
2021-06-08 15:30:13 -0300

    created buildings components

---
+++ client/src/components/BuildingsListView/BuildingsListView.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function BuildingsListView() {
+    return (
+        <div>
+            Here you will see a list of buildings.
+        </div>
+    );
+}
+
+export default BuildingsListView;
