9a75d9a31f053a8b6e350978ed1aa60db444200c
Leopoldo <leopoldoblanco@gmail.com>
2021-06-09 00:17:14 -0300

    change import styles and h2.component style

--- client/src/components/Buildings/Buildings.jsx
+++ client/src/components/Buildings/Buildings.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import BuildingsTable from '../BuildingsTable/BuildingsTable';
-import styles from './styles.css'
 import Container from '@material-ui/core/container';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import './styles.css';
 
 function Buildings() {
     return (
         <Container>
             <div className="componentHeader">
-                <Typography variant="h2" className="componentHeading">
+                <Typography variant="h2" className="componentHeading1">
                     Todos los edificios
                 </Typography>
                 <Button variant="contained" color="primary">Crear nuevo</Button>
