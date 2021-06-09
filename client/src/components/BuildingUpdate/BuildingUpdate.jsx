6f0defa6f4e4e2d720eeda33b40ab277aaad6a7e
Brian <brianrosarino@gmail.com>
2021-06-09 08:24:51 -0300

    Component created without finish

--- client/src/components/BuildingUpdate/BuildingUpdate.jsx
+++ client/src/components/BuildingUpdate/BuildingUpdate.jsx
@@ -4,6 +4,15 @@ import { useEffect, useState } from "react";
 import { getBuildingDetail, putBuilding } from "../../redux/building/buildingActions";
 
 function BuildingUpdate(props) {
+    const initialState = {
+        Name: false,
+        Address: false,
+        Catastral: false,
+        Floors:  false,
+        Apartments: false
+    }
+
+    const [editMode, setEditMode] = useState(initialState);
 
     const Building = {
         id:1,
@@ -15,25 +24,52 @@ function BuildingUpdate(props) {
         createdAt:"2021-06-08T18:42:58.276Z",
         updatedAt:"2021-06-08T18:42:58.276Z"
     }
-
+/* 
     useEffect(async () => {
         let prueba = await props.getBuildingDetail(1);
         console.log(prueba)
-    }, [])
+    }, []) */
 
+    const editModestatus = (change) => {
+        if(!editMode[change]){
+            return <h5>{change}: {Building[change.toLowerCase()]}</h5>
+        }else{
+            return <input value={Building[change.toLowerCase()]}></input>
+        }
+    }
+
+    const changeModeStatus = (e) => {
+        const toChange = e.target.id;
+        console.log(Object.assign(editMode, editMode[toChange] = !editMode[toChange]))
+        setEditMode(Object.assign(editMode, editMode[toChange] = editMode[toChange]))
+    }
 
     return (
         <div>
             <h2 id="header">Update your building info:</h2>
             <div id="DarkGrey">
-                <h3>
-                    Building: {Building.name}
-                </h3>
+                <div>
+                    {editModestatus("Name")}
+                    <h3>Resultado: {"" + editMode.Name}</h3>
+                <button id="Name" onClick={changeModeStatus}>EDIT</button>
+                </div>
                 <div id="DetailsBox">
+                    <div>
                     <h5>Address: {Building.address}</h5>
+                    <button>EDIT</button>
+                    </div>
+                    <div>
                     <h5>Catastral: {Building.cata}</h5>
+                    <button>EDIT</button>
+                    </div>
+                    <div>
                     <h5>Floors: {Building.floor}</h5>
+                    <button>EDIT</button>
+                    </div>
+                    <div>
                     <h5>Apartments: {Building.apartments}</h5>
+                    <button>EDIT</button>
+                    </div>
                 </div>
                 <button>SAVE CHANGES</button>
             </div>
@@ -44,7 +80,7 @@ function BuildingUpdate(props) {
 
 function mapStateToProps(state) {
     return {
-        pokemons: state.pokemons
+        detailBuilding: state.detailBuilding
     };
 }
 
