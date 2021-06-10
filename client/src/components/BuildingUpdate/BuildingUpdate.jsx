6d50be4bab791346b90934d1751f460b17c0f6cd
Brian <brianrosarino@gmail.com>
2021-06-10 14:42:55 -0300

    Floors and Apartments only accepts numbers

--- client/src/components/BuildingUpdate/BuildingUpdate.jsx
+++ client/src/components/BuildingUpdate/BuildingUpdate.jsx
@@ -1,9 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
-import { useEffect, useState , React} from "react";
+import { useEffect, useState, React } from "react";
 import { getBuildingDetail, putBuilding } from "../../redux/building/buildingActions";
 import { useParams } from "react-router-dom";
-import { Button , TextField, Container, Box} from '@material-ui/core';
-import  BusinessIcon  from '@material-ui/icons/Business';
+import { Button, TextField, Container, Box } from '@material-ui/core';
+import BusinessIcon from '@material-ui/icons/Business';
 import LocationOnIcon from '@material-ui/icons/LocationOn';
 import MeetingRoomIcon from '@material-ui/icons/MeetingRoom';
 import ListAltOutlinedIcon from '@material-ui/icons/ListAltOutlined';
@@ -23,19 +23,35 @@ function BuildingUpdate() {
         name: false,
         address: false,
         cata: false,
-        floor:  false,
+        floor: false,
         apartments: false
     });
-    
+
+    const [error, setError] = useState({
+        floor: false,
+        apartments: false,
+        name: false,
+        address: false,
+        cata: false
+    })
+
+    const [warning, setWarning] = useState({
+        floor: "",
+        apartments: "",
+        name: "",
+        address: "",
+        cata: ""
+    })
+
     const Building = {//Initial state for the inputs
-        cata:"",
-        floor:"",
-        apartments:"",
-        name:"",
-        address:""
+        cata: "",
+        floor: "",
+        apartments: "",
+        name: "",
+        address: ""
     }
-    
-    
+
+
     const [input, setInput] = useState({//Control the user inputs for every input
         name: Building.name,
         cata: Building.cata,
@@ -43,24 +59,39 @@ function BuildingUpdate() {
         apartments: Building.apartments,
         address: Building.address,
     });
+    const reg = new RegExp('^[0-9]+$')
 
-    const inputHandler = (change, text) =>{//input handler to change the state when the user write
-        setInput({
-            ...input,
-            [change]: text
-        })
+    const inputHandler = (change, text) => {//input handler to change the state when the user write
+        if ((change === "floor" || change === "apartments") && !reg.test(text)) {
+            setWarning({
+                ...warning,
+                [change]: "Solo puedes ingresar numeros!"
+            })
+            setError({
+                ...error,
+                [change]: true
+            })
+        }
+        if ((change !== "floor" && change !== "apartments") || reg.test(text) || text === "") {//just numbers in floor and apartments
+            setInput({
+                ...input,
+                [change]: text
+            })
+        }
     }
 
 
+
+
     const editModestatus = (change) => {//shows the data according to the status mode (read, write or preview )
-        if(!editMode[change]){//if is set to read mode
-            if(input[change] === Building[change]){//shows the current value if the user didn't write anything yet
+        if (!editMode[change]) {//if is set to read mode
+            if (input[change] === Building[change]) {//shows the current value if the user didn't write anything yet
                 return <h2>{translate[change]}: {Build.detailBuilding[0] && Build.detailBuilding[0][change.toLowerCase()]}</h2>
-            }else{//shows the preview of the changes if the user did write something
+            } else {//shows the preview of the changes if the user did write something
                 return <h2>{translate[change]}: {input[change]}</h2>
             }
-        }else{//if is set to write mode
-            return <TextField label={translate[change]} onChange={(e) => inputHandler(change, e.target.value)} value={input[change]} />
+        } else {//if is set to write mode
+            return <TextField error={error[change]} helperText={warning[change]} variant="outlined" label={translate[change]} onChange={(e) => inputHandler(change, e.target.value)} value={input[change]} />
         }
     }
 
@@ -74,6 +105,13 @@ function BuildingUpdate() {
 
     const saveHandler = (e) => {//send the data to change in the data base
         e.preventDefault();
+        setError({
+            floor: false,
+            apartments: false,
+            name: false,
+            address: false,
+            cata: false
+        })
         setInput({//set all the inputs to the initial state
             name: Building.name,
             cata: Building.cata,
@@ -85,7 +123,7 @@ function BuildingUpdate() {
             name: false,
             address: false,
             cata: false,
-            floor:  false,
+            floor: false,
             apartments: false
         })
 
@@ -96,45 +134,45 @@ function BuildingUpdate() {
             apartments: input.apartments || Build.detailBuilding[0].apartments,
             name: input.name || Build.detailBuilding[0].name,
             address: input.address || Build.detailBuilding[0].address
-    }))
-    .then(() => dispatch(getBuildingDetail(id)))//re render the info of the component and now the changes are the curren data
+        }))
+            .then(() => dispatch(getBuildingDetail(id)))//re render the info of the component and now the changes are the curren data
     }
 
     return (
         <Container maxWidth="sm">
-        <form noValidate autoComplete="off" onSubmit={saveHandler} >
-            <h1 id="header">Modificar edificio:</h1>
-            <div id="DarkGrey">
-            <Box display="flex">
-                <BusinessIcon />
-            {editModestatus("name")}
-                <Button variant="contained" name="name" onClick={changeModeStatus}>EDITAR</Button>
-                </Box>
-                <div id="DetailsBox">
-                <Box display="flex">
-                    <LocationOnIcon/>
-                    {editModestatus("address")}
-                    <Button variant="contained" name="address" onClick={changeModeStatus}>EDITAR</Button>
-                    </Box>
+            <form noValidate autoComplete="off" onSubmit={saveHandler} >
+                <h1 id="header">Modificar edificio:</h1>
+                <div id="DarkGrey">
                     <Box display="flex">
-                        <FormatAlignJustifyOutlinedIcon/>
-                    {editModestatus("cata")}
-                    <Button variant="contained" name="cata" onClick={changeModeStatus}>EDITAR</Button>
+                        <BusinessIcon fontSize="large" />
+                        {editModestatus("name")}
+                        <Button variant="contained" name="name" onClick={changeModeStatus}>EDITAR</Button>
                     </Box>
-                    <Box display="flex">
-                    <ListAltOutlinedIcon/>
-                    {editModestatus("floor")}
-                    <Button variant="contained" name="floor" onClick={changeModeStatus}>EDITAR</Button>
-                    </Box>
-                    <Box display="flex">
-                    <MeetingRoomIcon/>
-                    {editModestatus("apartments")}
-                    <Button variant="contained" name="apartments" onClick={changeModeStatus}>EDITAR</Button>
-                    </Box>
+                    <div id="DetailsBox">
+                        <Box display="flex">
+                            <LocationOnIcon fontSize="large" />
+                            {editModestatus("address")}
+                            <Button variant="contained" name="address" onClick={changeModeStatus}>EDITAR</Button>
+                        </Box>
+                        <Box display="flex">
+                            <FormatAlignJustifyOutlinedIcon fontSize="large" />
+                            {editModestatus("cata")}
+                            <Button variant="contained" name="cata" onClick={changeModeStatus}>EDITAR</Button>
+                        </Box>
+                        <Box display="flex">
+                            <ListAltOutlinedIcon fontSize="large"/>
+                            {editModestatus("floor")}
+                            <Button variant="contained" name="floor" onClick={changeModeStatus}>EDITAR</Button>
+                        </Box>
+                        <Box display="flex">
+                            <MeetingRoomIcon fontSize="large"/>
+                            {editModestatus("apartments")}
+                            <Button variant="contained" name="apartments" onClick={changeModeStatus}>EDITAR</Button>
+                        </Box>
+                    </div>
+                    <Button variant="contained" color="primary" onClick={saveHandler} >Guardar Cambios</Button>
                 </div>
-                <Button variant="contained" color="primary" onClick={saveHandler} >Guardar Cambios</Button>
-            </div>
-        </form>
+            </form>
         </Container>
     );
 }
