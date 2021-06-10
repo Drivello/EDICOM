7e21f858d1036546380e6af99a2fa0fa87219d37
Marcelo Fernandez <marceloleandrofernandez@gmail.com>
2021-06-10 17:16:03 -0300

    added dome material magic

--- client/src/components/ApartmentAdd/CreateApartmentForm.jsx
+++ client/src/components/ApartmentAdd/CreateApartmentForm.jsx
@@ -1,11 +1,18 @@
+import {makeStyles, Grid, Button, TextField} from '@material-ui/core'
+import { Domain, Room, LocationCity, Receipt, ListAlt, MeetingRoom,HomeWork } from '@material-ui/icons';
 
-
-
+const useStyles = makeStyles({
+    root: {
+   		margin: 50,
+	},
+  });
 const CreateApartmentForm = ({allBuildings, handleInputChange, handleSubmit, input}) => {
+	const classes = useStyles();
 
     return (
         <>
             <h1>Desde ApartmentForm</h1>
+			{/* 
 			<form>
 				<select>
 					<option value="" key="">
@@ -69,8 +76,77 @@ const CreateApartmentForm = ({allBuildings, handleInputChange, handleSubmit, inp
 				/>{' '}
 				building
 				<input type="button" value="CREAR" onClick={handleSubmit} />
-			</form>
-        </>
+			</form> 
+			*/}
+			<form action="">
+			<Grid container direction="row" justify="flex-start" alignItems="flex-start" className={`componentDataBox ${classes.root}`} spacing={1}>
+                <Grid item xs={6}>
+                    <Grid container spacing={1} alignItems="flex-end">
+                        <Grid item>
+                            <Domain />
+                        </Grid>
+                        <Grid item>
+                            <TextField id="apartment-floor" 
+										label="Piso" 
+										defaultValue="Piso" 
+
+							/>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={1} alignItems="flex-end">
+                        <Grid item>
+                            <Room />
+                        </Grid>
+                        <Grid item>
+                            <TextField id="building-address" label="Dirección" defaultValue="Calle y número" />
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={1} alignItems="flex-end">
+                        <Grid item>
+                            <HomeWork />
+                        </Grid>
+                        <Grid item>
+                            <TextField id="building-city" label="Ciudad" defaultValue="Ciudad" />
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid item xs={6}>
+                    <Grid container spacing={1} alignItems="flex-end">
+                        <Grid item>
+                            <MeetingRoom />
+                        </Grid>
+                        <Grid item>
+                            <TextField id="apartment-number"
+									   label="Nº Departamento" 
+									   defaultValue="Número Departamento" 
+							/>
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={1} alignItems="flex-end">
+                        <Grid item>
+                            <ListAlt />
+                        </Grid>
+                        <Grid item>
+                            <TextField id="building-floors" type="number" label="Pisos" defaultValue="1" />
+                        </Grid>
+                    </Grid>
+                    <Grid container spacing={1} alignItems="flex-end">
+                        <Grid item>
+                            <MeetingRoom />
+                        </Grid>
+                        <Grid item>
+                            <TextField id="building-apartments" type="number" label="Departamentos" defaultValue="1" />
+                        </Grid>
+                    </Grid>
+                </Grid>
+                <Grid container direction="row" justify="flex-start" alignItems="flex-start">
+                    <Grid item>
+                        <Button variant="contained" color="primary">Confirmar</Button>
+                    </Grid>
+                </Grid>
+            </Grid>
+        </form>
+		</>
     )
 
 
