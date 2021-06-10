ad8d44b39c35b7f7c60b0ec966e89ebebe4bda6f
MauriiAC <mauriciocuello91@gmail.com>
2021-06-10 10:24:05 -0300

    putSpendingController added in index

--- api/src/routes/spendings.js
+++ api/src/routes/spendings.js
@@ -3,10 +3,13 @@ const express = require('express');
 
 const allSpendings = require("../controllers/spendings/allSpendingsControllers");     
 const addSpendings = require("../controllers/spendings/addSpendingsController");     
+const putSpendings = require("../controllers/spendings/putSpendingsController");     
 
 router.use(express.json());
 router.get("/all", allSpendings);                   //      http://localhost:3001/spendings/all
 router.post("/add", addSpendings);                  //      http://localhost:3001/spendings/add
+router.put("/add", putSpendings);                  //      http://localhost:3001/spendings/add
+
 
 router.get('/', async function(req,res,next){       //      endPoint
     console.log("Estoy en la ruta /spendings")
