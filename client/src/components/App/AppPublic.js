a20035fa614f35a474ce1eec1ae8ab9502702470
Gabi Fresco <gabi94f@gmail.com>
2021-06-25 09:00:26 -0300

    booking component

--- client/src/components/App/AppPublic.js
+++ client/src/components/App/AppPublic.js
@@ -4,20 +4,19 @@ import {BrowserRouter} from 'react-router-dom';
 import {Route} from 'react-router';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
-import { Header } from '../HomeTenant/Header';
+import {Header} from '../HomeTenant/Header';
 import Sidebar from '../Sidebar/Sidebar';
-
+import Bookings from '../Users/UserView/Bookings/Bookings';
 
 function AppPublic() {
 	return (
 		<Container className="App">
 			<CssBaseline />
-				<BrowserRouter>
-
-					//Poner acá los route con los componentes de la vista del locatario
-					<Route path="/public" component={Sidebar} />
-
-				</BrowserRouter>
+			<BrowserRouter>
+				//Poner acá los route con los componentes de la vista del locatario
+				<Route path="/public" component={Sidebar} />
+				<Route path="/public/Bookings" component={Bookings} />
+			</BrowserRouter>
 		</Container>
 	);
 }
---
+++ client/src/components/Users/UserView/Bookings/Bookings.jsx
@@ -0,0 +1,9 @@
+const Bookings = () => {
+    return (
+        <>
+            <h1>Desde Booking</h1>
+        </>
+      );
+}
+ 
+export default Bookings;
