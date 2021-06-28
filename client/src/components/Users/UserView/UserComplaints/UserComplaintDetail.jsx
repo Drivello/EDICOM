2085a944e98313111b05c4011ba72a35cf2f6fa5
Leopoldo <leopoldoblanco@gmail.com>
2021-06-28 18:56:04 -0300

    fixed user id bugssss

---
+++ client/src/components/Users/UserView/UserComplaints/UserComplaintDetail.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function UserComplaintDetail(props) {
+    
+    console.log(props);
+
+    return (
+        <div>
+            I'm a User Complaint Detail
+        </div>
+    );
+}
+
+export default UserComplaintDetail;
