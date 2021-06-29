b4ad2c5a44315d39c13bd9a0c70fff48a47b7d8c
Brian <brianrosarino@gmail.com>
2021-06-29 12:31:09 -0300

    bugs fixed in UpdateAMenity

--- client/src/components/Amenities/UpdateAmenity.jsx
+++ client/src/components/Amenities/UpdateAmenity.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux';
 import { Button, TextField, makeStyles, Grid, } from '@material-ui/core';
-import styles from '../Alerts/AlertsUpdate.module.css'
+import styles from './UpdateAmenity.module.css'
 import { Person, Home } from '@material-ui/icons';
 import { getAmenityById, updateAmenity, deleteAmenity } from '../../redux/amenities/amenitiesActions'
 import { Link, useHistory } from 'react-router-dom';
@@ -93,10 +93,10 @@ export function UpdateAmenity() {
 	})
 
 	const helperInit = {
-			amenity_type: "Ingrese un Amenity",
-			quantity: "Ingrese la cantidad",
-			capacity: "Ingrese la capacidad",
-			amenity_detail: "Ingrese un Detalle",
+		amenity_type: "Ingrese un Amenity",
+		quantity: "Ingrese la cantidad",
+		capacity: "Ingrese la capacidad",
+		amenity_detail: "Ingrese un Detalle",
 	}
 
 
@@ -146,164 +146,101 @@ export function UpdateAmenity() {
 	}
 
 
-	const Validate = (field) => {
-		switch (field.name) {
-			case "amenity_type":
-				if (!/^[A-Za-z .'-]{3,20}$/.test(field.value)) {
-					setError({ ...error, name: true })
-					if (field.value.length < 3) { setHelperText({ ...helperText, name: "Es muy corto" }) }
-					else if (field.value.length > 20) { setHelperText({ ...helperText, name: "Es muy largo" }) }
-					else { setHelperText({ ...helperText, name: "No se permiten caracteres especiales" }) }
-				} else {
-					setError({ ...error, name: false })
-					setHelperText({ ...helperText, name: "" })
-				}
-				break;
-			case "quantity":
-				if (!/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(field.value)) {
-					setError({ ...error, email: true })
-					if (field.value.length < 3) { setHelperText({ ...helperText, email: "Es muy corto" }) }
-					else if (field.value.length > 20) { setHelperText({ ...helperText, email: "Es muy largo" }) }
-					else { setHelperText({ ...helperText, email: "Contiene caracteres no aceptados" }) }
-				}
-				else {
-					setError({ ...error, email: false })
-					setHelperText({ ...helperText, email: "" })
-				}
-				break;
-			case "amenity_detail":
-				if (!/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,60}$/.test(field.value)) {
-					setError({ ...error, password: true })
-					if (field.value.length < 8) { setHelperText({ ...helperText, password: "Es muy corto" }) }
-					else if (field.value.length > 60) { setHelperText({ ...helperText, password: "Es muy largo" }) }
-					else { setHelperText({ ...helperText, password: "1 nro, 1 mayus y 1 min" }) }
-				}
-				else {
-					setError({ ...error, password: false })
-					setHelperText({ ...helperText, password: "" })
-				}
-				break;
-
-			default:
-				break;
-		}
-	}
-
-
-
 	return (
 		<ThemeProvider theme={theme}>
-			<>
+			<div className="extContCAF">
+				<h1>Editar Amenity</h1>
 				<form noValidate autoComplete="off" >
-					<Grid container direction="row" justify="space-around" alignItems="center" className={`componentDataBox ${classes.root}`} spacing={1}>
-						<Grid item xs={6}>
-							<Grid container spacing={1} alignItems="center" justify="center">
-								<Grid>
-									<Domain fontSize="large" />
-								</Grid>
-								<Grid item>
-									<TextField
-										error={error["amenity_type"]}
-										variant="outlined"
-										helperText={[helperText["amenity_type"]]}
-										id="amenity_type"
-										label="Amenity"
-										name="amenity_type"
-										value={input.amenity_type || ''}
-										onChange={(e) => handleInputChange(e, "amenity_type")}
-									/>
-								</Grid>
-							</Grid>
+					<div className={styles.form}>
+						<div className={styles.left}>
+							<div className={styles.item}>
+								<Domain fontSize="large" />
+								<TextField
+									error={error["amenity_type"]}
+									variant="outlined"
+									helperText={[helperText["amenity_type"]]}
+									id="amenity_type"
+									label="Amenity"
+									name="amenity_type"
+									value={input.amenity_type || ''}
+									onChange={(e) => handleInputChange(e, "amenity_type")}
+								/>
+							</div>
+							<div className={styles.item}>
+								<FormatListNumberedIcon fontSize="large" />
+								<TextField
+									variant="outlined"
+									error={error["quantity"]}
+									helperText={[helperText["quantity"]]}
+									id="quantity"
+									label="Cantidad"
+									name='quantity'
+									value={input.quantity || ''}
+									onChange={(e) => handleInputChange(e, "quantity")}
+								/>
+							</div>
+						</div>
+						<div className={styles.left}>
+							<div className={styles.item}>
+								<PeopleAltIcon fontSize="large" />
+								<TextField
+									variant="outlined"
+									error={error["capacity"]}
+									helperText={[helperText["capacity"]]}
+									id="capacity"
+									label="Capacidad"
+									name='capacity'
+									value={input.capacity || ''}
+									onChange={(e) => handleInputChange(e, "capacity")}
+								/>
+							</div>
+							<div className={styles.item}>
+								<AssignmentIcon fontSize="large" />
+								<TextField
+									multiline={true}
+									rowsMax={4}
+									variant="outlined"
+									error={error["amenity_detail"]}
+									helperText={[helperText["amenity_detail"]]}
+									id="amenity_detail"
+									label="Detalle"
+									name='amenity_detail'
+									value={input.amenity_detail || ''}
+									onChange={(e) => handleInputChange(e, "amenity_detail")}
+								/>
+							</div>
+						</div>
+					</div>
 
-							<Grid container spacing={1} alignItems="center" justify="center">
-								<Grid item>
-									<FormatListNumberedIcon fontSize="large" />
-								</Grid>
-								<Grid item>
-									<TextField
-										variant="outlined"
-										error={error["quantity"]}
-										helperText={[helperText["quantity"]]}
-										id="quantity"
-										label="Cantidad"
-										name='quantity'
-										value={input.quantity || ''}
-										onChange={(e) => handleInputChange(e, "quantity")}
-									/>
-								</Grid>
-							</Grid>
-
-							<Grid container spacing={1} alignItems="center" justify="center">
-								<Grid item>
-									<PeopleAltIcon fontSize="large" />
-								</Grid>
-								<Grid item>
-									<TextField
-										variant="outlined"
-										error={error["capacity"]}
-										helperText={[helperText["capacity"]]}
-										id="capacity"
-										label="Capacidad"
-										name='capacity'
-										value={input.capacity || ''}
-										onChange={(e) => handleInputChange(e, "capacity")}
-									/>
-								</Grid>
-							</Grid>
-
-							<Grid container spacing={1} alignItems="center" justify="center">
-								<Grid item>
-									<AssignmentIcon fontSize="large" />
-								</Grid>
-								<Grid item>
-									<TextField
-										variant="outlined"
-										error={error["amenity_detail"]}
-										helperText={[helperText["amenity_detail"]]}
-										id="amenity_detail"
-										label="Detalle"
-										name='amenity_detail'
-										value={input.amenity_detail || ''}
-										onChange={(e) => handleInputChange(e, "amenity_detail")}
-									/>
-								</Grid>
-							</Grid>
-
-						</Grid>
-						<Grid container direction="row" justify="center" alignItems="center">
-							<Grid item>
-								<Button
-									id={styles.submit}
-									style={{ fontWeight: 1000, marginTop: 50 }}
-									color="secondary"
-									onClick={handleSubmit}
-									variant="contained"
-								>
-									Guardar Cambios
-								</Button>
-								<Button
-									id={styles.submit}
-									style={{ fontWeight: 1000, marginTop: 50 }}
-									color="primary"
-									variant="contained"
-									onClick={deleteHandler}
-								>
-									Eliminar Amenity
-								</Button>
-								<Button
-									id={styles.submit}
-									style={{ fontWeight: 1000, marginTop: 50 }}
-									color="primary"
-									variant="contained"
-									onClick={cancelHandler}
-								>
-									Cancelar
-								</Button>
-							</Grid>
-						</Grid>
-					</Grid>
+					<Button
+						id={styles.submit}
+						style={{ fontWeight: 1000, marginTop: 50 }}
+						color="secondary"
+						onClick={handleSubmit}
+						variant="contained"
+					>
+						Guardar Cambios
+					</Button>
+					<Button
+						id={styles.submit}
+						style={{ fontWeight: 1000, marginTop: 50 }}
+						color="primary"
+						variant="contained"
+						onClick={deleteHandler}
+					>
+						Eliminar Amenity
+					</Button>
+					<Button
+						id={styles.submit}
+						style={{ fontWeight: 1000, marginTop: 50 }}
+						color="primary"
+						variant="contained"
+						onClick={cancelHandler}
+					>
+						Cancelar
+					</Button>
 				</form>
-			</>
+			</div>
 		</ThemeProvider>
 	)
 }
