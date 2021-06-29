4debd5fe7a450d32051d92474e9e1610c8912ae9
MauriiAC <mauriciocuello91@gmail.com>
2021-06-29 08:52:43 -0300

    fix problem with new page in payment
    Co-authored-by: MarianoGuillon <MarianoGuillon@users.noreply.github.com>

--- client/src/redux/payments/paymentsActions.js
+++ client/src/redux/payments/paymentsActions.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 export const POST_PAYMENT = 'POST_PAYMENT';
+export const RESET_URL_PAYMENT = 'RESET_URL_PAYMENT'
 
 
 //http://localhost:3001/ratings
@@ -19,5 +20,12 @@ export const addPayment = (title, price, quantity = 1) =>  {
         .catch(err => {
             console.log(err)
         })
+    }
+}
+
+export const resetUrlPayment = () =>  {
+
+    return {
+        type: RESET_URL_PAYMENT,
     }
 }
