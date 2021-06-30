52b0c63163813557250ef3e3d70cb8afcf5e435e
florencia caballero <florcha12.caballero@gmail.com>
2021-06-30 10:20:40 -0300

    local changes

--- client/src/components/Spending/Form.jsx
+++ client/src/components/Spending/Form.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useHistory, useParams } from 'react-router';
+import { useParams } from 'react-router';
 import {
   postSpending,
   putSpending,
